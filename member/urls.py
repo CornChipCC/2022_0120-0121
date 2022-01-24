d041d06795352c9e958b3258a399b585a2b7fe8a
ConChipCC <sgchfh@naver.com>
2022-01-24 09:38:40 +0900

    0124

--- member/urls.py
+++ member/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
+
 app_name = 'member'
 urlpatterns = [
     path('login/',
@@ -12,4 +14,5 @@ urlpatterns = [
         auth_views.LogoutView.as_view(),
         name='logout'
     ),
+    path('signup/', views.signup, name='signup'),
 ]
